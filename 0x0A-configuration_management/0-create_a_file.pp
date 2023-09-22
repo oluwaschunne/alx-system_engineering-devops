c000c2ce958fd3b614da2b91f0e2edcea08d9b9e
oluwaschunne <falopeoluwaseun2@gmail.com>
2023-09-22 11:57:29 -0700

    p-file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# create a file in /tmp
+# Create file in /tmp
 
 file { '/tmp/school' :
-	path	=> '/tmp/school',
-	mode	=> '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love Puppet',
-	}
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
